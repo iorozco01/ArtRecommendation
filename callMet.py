e7a4908422a70d4401ba3d631c8fbc6002c99dc3
iorozco01 <ivanorozco9@gmail.com>
2024-06-30 21:44:24 -0700

    Implemented code to download art from Met
    callMet will now iterate through the entire list of artworks in the Met repo and download all viable options. Removed get random artwork for now

--- callMet.py
+++ callMet.py
@@ -1,10 +1,17 @@
 import random
 import requests
-from PIL import Image
+import os
+import json
+from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 
 # Met API URL
 url = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+
+# Create directories to save images and metadata
+os.makedirs('met_images', exist_ok=True)
+os.makedirs('met_metadata', exist_ok=True)
+
 
 # Get object with ID from Met API
 def search_object(object_id):
@@ -17,28 +24,47 @@ def search_object(object_id):
         if object_details.get('isPublicDomain', True):
             return object_details
         else:
-            print("Object number " + str(object_id) + " is not in the public domain).")
+            print(f"Artwork with ID {object_id} is not in the public domain.")
     else:
-        print("Object number " + str(object_id) + " not found.")
+        print(f"Artwork with ID {object_id} not found.")
 
-    # Retry with a random object ID if the object is not found or not in public domain
-    return search_object(str(random.randint(object_ids[0], object_ids[-1])))
+    # Return None if object in location doesn't fulfill parameters
+    return None
+
+
+# Get image of artwork from API and download
+def download_image(art_url, image_):
+    img_response = requests.get(art_url)
+    if img_response.status_code == 200:
+        try:
+            img = Image.open(BytesIO(img_response.content))
+            img.save(image_path)
+            print(f"Image saved to {image_path}")
+        except UnidentifiedImageError:
+            print(f"Failed to identify image from URL: {art_url}")
+    else:
+        print(f"Failed to download image from URL: {art_url}, status code: {img_response.status_code}")
 
 
 response = requests.get(url)
 data = response.json()
 object_ids = data['objectIDs']
 
-# object_num not accurate if change is made in search_object
-artwork = search_object(random.randint(object_ids[0], object_ids[-1]))
-if artwork:
-    artwork_img_url = artwork.get('primaryImage')
-    print("Artwork details:", artwork)
-    print("Primary image URL:", artwork_img_url)
+for art_id in object_ids:
+    artwork = search_object(art_id)
+    if artwork:
+        # Save metadata
+        metadata_path = os.path.join('met_metadata', f"{art_id}.json")
+        with open(metadata_path, 'w') as m:
+            json.dump(artwork, m)
 
-# Display image
-    img_response = requests.get(artwork_img_url)
-    img = Image.open(BytesIO(img_response.content))
-    img.show()
-else:
-    print("No artwork found.")
+        # Save image
+        artwork_img_url = artwork.get('primaryImage')
+        if artwork_img_url:
+            image_path = os.path.join('met_images', f"{art_id}.jpg")
+            download_image(artwork_img_url, image_path)
+            print(f"Artwork with ID {art_id} downloaded.")
+        else:
+            print(f"Artwork with ID {art_id} has no primary image.")
+    else:
+        print(f"No artwork found for ID {art_id}")
